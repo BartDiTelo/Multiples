f12cdccf49c45e702b6df88c0c6bbfc325f9cc57
Bartłomiej Dietl <bartlomiejdietl@Barts-MacBook-Pro.local>
2015-11-08 23:32:55 +0100

    final commit

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var multipier: Int = 0
+    var currenValue: Int = 0
+    let maxVale: Int = 50
+    
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var entryField: UITextField!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,8 +30,44 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    
     }
-
+    
+    @IBAction func onPlayButtonPressed(sender: UIButton!) {
+        if entryField.text != nil && entryField.text != "" {
+            startApp()
+        }
+    }
+    
+    @IBAction func onAddClick(sender: UIButton!) {
+        currenValue = currenValue + multipier
+        if currenValue >= maxVale {
+            gameOver()
+        } else {
+            resultLabel.text = "\(currenValue - multipier) + "+"\(multipier) ="+"\(currenValue)"
+        }
+    }
+    
+    func gameOver() {
+        playButton.hidden = false
+        logo.hidden = false
+        entryField.hidden = false
+        addButton.hidden = true
+        resultLabel.hidden = true
+        entryField.text = ""
+        
+    }
+    func startApp() {
+        playButton.hidden = true
+        logo.hidden = true
+        entryField.hidden = true
+        addButton.hidden = false
+        resultLabel.hidden = false
+        
+        currenValue = 0
+        multipier = Int(entryField.text!)!
+        resultLabel.text = "Press ADD to add!"
+    }
 
 }
 
